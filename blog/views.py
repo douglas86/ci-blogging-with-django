841d932f368a051147d3e37634d014a0a9ce0741
douglas <douglasmaxton@gmail.com>
2024-01-12 14:19:50 +0000

    added a filter to filter for all published posts on a model

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.views import generic
 from .models import Post
 
 """
+generic views - 
+    Generic views are beneficial for dealing with repetitive full-stck coding tasks such as displaying database contents
+    to a webpage. It handles teh most common use cases in web app development.
 generic.ListView - 
     The advantage of using this type of view is that you don't have to add the HTML  template only the model
 """
@@ -10,5 +13,5 @@ generic.ListView -
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = 'post_list.html'
