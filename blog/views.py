3b29a4755d782d684395eb97e14361206c80cb46
douglas <douglasmaxton@gmail.com>
2024-01-23 16:43:32 +0000

    adds post to page when submit clicked

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,23 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    context = {'post': post, 'coder': 'Douglas Maxton'}
+    comments = post.comments.all().order_by('-created_on')
+    comments_count = post.comments.filter(approved=True).count()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Comment submitted and awaiting approval'
+            )
+
+    comment_form = CommentForm()
+    context = {'post': post, 'comments': comments, 'comment_count': comments_count, 'comment_form': comment_form}
 
     return render(
         request, "blog/post_detail.html", context
