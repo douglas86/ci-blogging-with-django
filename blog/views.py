ccdd35855e678a83bf1a92dc2ef9141e2a2dee10
douglas <douglasmaxton@gmail.com>
2024-01-23 11:58:09 +0000

    created the view and the url for commenting

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
-from .models import Post
+from .models import Post, Comment
+from django.http import HttpResponseRedirect
 
 """
 generic views - 
@@ -56,3 +57,24 @@ def post_detail(request, slug):
     return render(
         request, "blog/post_detail.html", context
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == 'POST':
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = form = CommentForm(date=request.POST, instance=comment)
+
+        if commennt_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_messages(request, messages.SUCCESS, 'Your comment has been updated!')
+        else:
+            message.add_messages(request, messages.ERROR, 'Error updating commit!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug])
